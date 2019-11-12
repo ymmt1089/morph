1675aa89f13624a6a9074a15092aa522986cc365
ymmt1089 <ymmt1089@gmail.com>
2019-11-12 09:13:29 +0000

    フラッシュメッセージ日本語化

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,6 +14,7 @@ module Morph
     config.load_defaults 5.2
     config.active_record.default_timezone = :local
     config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
