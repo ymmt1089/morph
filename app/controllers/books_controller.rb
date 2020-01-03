95fae55cbbe7891a43d389bed86e928ad64738a1
ymmt1089 <ymmt1089@gmail.com>
2020-01-03 08:04:19 +0000

    コントローラ変数修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,7 +20,7 @@ class BooksController < ApplicationController
 		@books.each do |book|
 			one_book_morpheme_origins = Morpheme.where("(pos like ? or pos like ? or pos like ? or pos like ? or pos like ? or pos like ? ) and origin not like ? and origin not like ? and origin not like ? and origin not like ? and origin not like ? and book_id = ? ","名詞-一般","%名詞-固有名詞%","名詞-副詞可能","名詞-接尾-人名","名詞-接尾-地域","動詞-自立","する","ある","なる","いう","いる", book.id)
 			one_book_morpheme_origins_count = one_book_morpheme_origins.group(:origin).count
-			one_book_morpheme_origins_count_sorted_hash = Hash[one_book_morpheme_origins_count.sort_by{ |_, v| -v } ] #hash化及び、valueの昇順(DESC)でソートする
+			one_book_morpheme_origins_count_sorted_hash = Hash[one_book_morpheme_origins_count.sort_by{ |key,value| -value }  ] #hash化及び、valueの昇順(DESC)でソートする
 			result = one_book_morpheme_origins_count_sorted_hash.reject{|key,value|(/nil/ =~ key) || (value <= (Math.sqrt(one_book_morpheme_origins_count_sorted_hash.first[1])/2))}
 			changed_result = result.map{|v| {text:v[0],size:v[1]}}
 			words = changed_result.to_json.html_safe
@@ -95,7 +95,7 @@ class BooksController < ApplicationController
 		# 感情分析用配列
 		one_book_morpheme_origins_all = Morpheme.where("(pos not like ? and pos not like ? and origin not like ? and origin not like ? and origin not like ? and origin not like ? and origin not like ?) and book_id = ?","%記号%","助詞%","する","ある","なる","いう","いる" ,@book.id)
 		one_book_morpheme_origins_count_all = one_book_morpheme_origins_all.group(:origin).count
-		one_book_morpheme_origins_count_sorted_hash_all = Hash[one_book_morpheme_origins_count_all.sort_by{ |_, v| -v } ]
+		one_book_morpheme_origins_count_sorted_hash_all = Hash[one_book_morpheme_origins_count_all.sort_by{ |key,value| -value }  ]
 		result_all = one_book_morpheme_origins_count_sorted_hash_all.reject{|key,value|(/nil/ =~ key) || (value <= 0)}
 		result_all_map = result_all.map{|v| {text:v[0],size:v[1]}}
 		sentimental_result = result_all_map.compact
--- app/controllers/morphemes_controller.rb
+++ app/controllers/morphemes_controller.rb
@@ -16,8 +16,8 @@ class MorphemesController < ApplicationController
 		@meishis_array = []
 		one_book_morpheme_meishi = Morpheme.where("(pos like ? or pos like ? or pos like ? or pos like ? or pos like ?) and book_id = ?","名詞-一般","%名詞-固有名詞%","名詞-副詞可能","名詞-接尾-人名","名詞-接尾-地域", @book.id)
 		one_book_morpheme_meishi_count = one_book_morpheme_meishi.group(:origin).count
-		one_book_morpheme_meishi_count_sorted_hash = Hash[one_book_morpheme_meishi_count.sort_by{ |_, v| -v } ]
-		result_meishi = one_book_morpheme_meishi_count_sorted_hash.reject{|key,value|(/nil/ =~ key) ||(value <= (Math.sqrt(one_book_morpheme_meishi_count_sorted_hash.first[1])/1.5))}
+		one_book_morpheme_meishi_count_sorted_hash = Hash[one_book_morpheme_meishi_count.sort_by{ |key,value| -value } ]
+		result_meishi = one_book_morpheme_meishi_count_sorted_hash.reject{|key,value|(/nil/ =~ key) || (value < 1)}
 		unless result_meishi.values.map(&:to_f).sum == 0
 			meishi_par = (100) / (result_meishi.values.map(&:to_f).sum)
 		end
@@ -36,7 +36,7 @@ class MorphemesController < ApplicationController
 		@doushis_array = []
 		one_book_morpheme_doushi = Morpheme.where("(pos like ? and origin not like ? and origin not like ? and origin not like ? and origin not like ? and origin not like ?) and book_id = ?","動詞-自立","する","ある","なる","いう","いる" ,@book.id)
 		one_book_morpheme_doushi_count = one_book_morpheme_doushi.group(:origin).count
-		one_book_morpheme_doushi_count_sorted_hash = Hash[one_book_morpheme_doushi_count.sort_by{ |_, v| -v } ]
+		one_book_morpheme_doushi_count_sorted_hash = Hash[one_book_morpheme_doushi_count.sort_by{ |key,value| -value } ]
 		result_doushi = one_book_morpheme_doushi_count_sorted_hash.reject{|key,value|(/nil/ =~ key) || (value < 1)}
 		unless result_doushi.values.map(&:to_f).sum == 0
 			doushi_par = (100) / (result_doushi.values.map(&:to_f).sum)
@@ -56,7 +56,7 @@ class MorphemesController < ApplicationController
 		@hinshis_array = []
 		one_book_morpheme_hinshi = Morpheme.where("book_id = ?", @book.id)
 		one_book_morpheme_hinshi_count = one_book_morpheme_hinshi.group(:pos).count
-		one_book_morpheme_hinshi_count_sorted_hash = Hash[one_book_morpheme_hinshi_count.sort_by{ |_, v| -v } ]
+		one_book_morpheme_hinshi_count_sorted_hash = Hash[one_book_morpheme_hinshi_count.sort_by{ |key,value| -value }  ]
 		result_hinshi = one_book_morpheme_hinshi_count_sorted_hash.reject{|key,value|(/nil/ =~ key) || (value < 1)}
 		unless result_hinshi.values.map(&:to_f).sum == 0
 			hinshi_par = (100) / (result_hinshi.values.map(&:to_f).sum)
@@ -71,6 +71,9 @@ class MorphemesController < ApplicationController
 		table_hinshi = table_hinshi.map{|v| {hinshi:v[0],count:v[1]}}
 		table_hinshi_changed_json = table_hinshi.to_json.html_safe
 		@table_hinshi_graph = table_hinshi_changed_json
+
+		binding.pry
+
 	end
 
 	def index
