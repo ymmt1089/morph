0393c271f54d672783f731e84e5cfbeea3555ce7
ymmt1089 <ymmt1089@gmail.com>
2020-01-06 07:18:39 +0000

    admin側削除機能実装途中

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,4 +5,13 @@ class AdminsController < ApplicationController
         @users = User.all
     end
 
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def edit
+        @user = User.find(params[:id])
+        @book = Book.find(params[:id])
+    end
+
 end
